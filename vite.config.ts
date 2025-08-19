e203b9eab4f9c8ddf15cfe51a5ff79df22e5f914
fahadshuvo33 <fahadshuvo33@gmail.com>
2025-08-20 00:45:31 +0600

    fix deployed error

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,26 @@
 import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueDevTools()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current directory.
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  // For GitHub Pages deployment
+  const isGitHubPages = process.env.NODE_ENV === 'production' && process.env.GITHUB_ACTIONS
+  
+  return {
+    base: isGitHubPages ? '/portfolio/' : '/',
+    plugins: [vue(), vueDevTools()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
+    define: {
+      __APP_ENV__: env.APP_ENV,
+    },
+  }
 })
